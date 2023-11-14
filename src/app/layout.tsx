e2c1482676f55c15c9de746201ef39e07da0eead
Sahilbalhra <sahilbalhra0@gmail.com>
2023-11-14 22:58:51 +0530

    some intro section changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from '@/components/Header'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Sahil | Personal Portfolio',
-  description: 'A full-stack developer with 1 year of experience',
-}
+    title: 'Sahil | Personal Portfolio',
+    description: 'A full-stack developer with 1 year of experience',
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}>
-        <div className='bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
-        <div className='bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
-        <Header />
-        {children}
-      </body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang='en'>
+            <body
+                className={`${inter.className} relative bg-gray-50 pt-28 text-gray-950 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 sm:pt-36`}>
+                <div className='absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]'></div>
+                <div className='absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+
+                <Header />
+                {children}
+            </body>
+        </html>
+    );
 }
--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -1,26 +1,47 @@
-import Image from 'next/image'
-import React from 'react'
-import IntroImg from '@/../public/IntroImg.jpg'
+'use client';
+import Image from 'next/image';
+import React from 'react';
+import { motion } from 'framer-motion';
+import IntroImg from '@/../public/IntroImg.jpg';
 
 const Intro = () => {
     return (
         <section>
             <div className='flex items-center justify-center'>
                 <div className='relative'>
-                    <Image
-                        src={IntroImg}
-                        alt='Sahil Image'
-                        width="192"
-                        height="192"
-                        quality="95"
-                        priority={true}
-                        className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
-                    />
-                    <span className='absolute bottom-0 text-4xl'>👋</span>
+                    <motion.div
+                        initial={{ opacity: 0, scale: 0 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{
+                            type: 'tween',
+                            duration: 0.2,
+                        }}>
+                        <Image
+                            src={IntroImg}
+                            alt='Sahil Image'
+                            width='192'
+                            height='192'
+                            quality='95'
+                            priority={true}
+                            className='h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl'
+                        />
+                        <motion.span
+                            className='absolute bottom-0 right-0 text-4xl'
+                            initial={{ opacity: 0, scale: 0 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{
+                                type: 'spring',
+                                stiffness: 125,
+                                delay: 0.1,
+                                duration: 0.7,
+                            }}>
+                            👋
+                        </motion.span>
+                    </motion.div>
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Intro
+export default Intro;
