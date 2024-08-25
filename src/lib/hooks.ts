e9c8e2c6bec44f57d72d8d42e24b2d57363a5d62
Sahilbalhra <sahilbalhra0@gmail.com>
2024-08-25 15:36:29 +0530

    fixed build issue

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,21 +1,23 @@
-import { useActiveSectionContext } from "@/context/active-section-context";
-import { useEffect } from "react";
-import { useInView } from "react-intersection-observer";
-import type { SectionName } from "./types";
+import { useEffect } from 'react';
 
-export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
-  const { ref, inView } = useInView({
-    threshold,
-  });
-  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+import { useActiveSectionContext } from '@/context/active-section-context';
+import { useInView } from 'react-intersection-observer';
 
-  useEffect(() => {
-    if (inView && Date.now() - timeOfLastClick > 1000) {
-      setActiveSection(sectionName);
-    }
-  }, [inView, setActiveSection, timeOfLastClick, sectionName]);
+// import type { SectionName } from "./types";
 
-  return {
-    ref,
-  };
+export function useSectionInView(sectionName: any, threshold = 0.75) {
+	const { ref, inView } = useInView({
+		threshold,
+	});
+	const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+
+	useEffect(() => {
+		if (inView && Date.now() - timeOfLastClick > 1000) {
+			setActiveSection(sectionName);
+		}
+	}, [inView, setActiveSection, timeOfLastClick, sectionName]);
+
+	return {
+		ref,
+	};
 }
